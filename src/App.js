bb66d9a1eace6564c4b4389d0df33593affdd9e6
Bregnballe <bregnballe@gmail.com>
2018-02-18 16:56:40 +0100

    Added styled components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
 
+
+const CardTitle = styled.h1`
+  font-size: 1.5em;
+  color: red;
+  text-align: center;
+`;
+//the const has to start with a capital letter
+//the const cannot have the same name as the component where it is used
+
+const CardContainer = styled.div`
+margin-top: 36px;
+padding-bottom: 36px;
+background-color: #FFF;
+border: 1px solid #e5e5e5;
+width: 400px;
+`;
+
+class Card extends Component {
+  render () {
+    return (
+      <CardContainer>
+        <CardTitle>This is my card title</CardTitle>
+      </CardContainer>
+    );
+  }
+}
+
+
 class App extends Component {
   render() {
-    let name = "Mattias"
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello, {name}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Card/>
     );
   }
 }
